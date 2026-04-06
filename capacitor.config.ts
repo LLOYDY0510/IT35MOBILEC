e2f6f5346157a0e6b33d9da5b19018c159cca925
JOHNLLOYD DONQUE <LLOYDY0510>
2026-04-06 20:53:19 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'it35mobileC',
+  webDir: 'dist'
+};
+
+export default config;
